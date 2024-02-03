0ec03a0ad39e45a4547fce79e88a10006197d36b
jelenu <jesusleon2700@gmail.com>
2024-02-03 13:07:14 +0100

    boardDetailView devuelve las cards ordenadas por el order

--- boards/serializers.py
+++ boards/serializers.py
@@ -13,6 +13,11 @@ class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
         fields = ['id', 'name', 'order', 'cards']
+        
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['cards'] = sorted(representation['cards'], key=lambda x: x['order'])
+        return representation
 
 class BoardSerializer(serializers.ModelSerializer):
     lists = ListSerializer(many=True, read_only=True)
